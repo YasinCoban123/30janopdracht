055b9edf7c3d32141c04597c748d80cb08bb5cab
YasinCoban123 <9018681@student.zadkine.nl>
2023-01-30 15:47:35 +0100

    ppp

--- htdocs/subpagina.php
+++ htdocs/subpagina.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link rel="stylesheet" href="subpagina.css">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>Home</title>
+  <title>subpagina</title>
 </head>
 <body>
   <nav>
     <ul>
-      <li><a href="subpagina.html">yasinn</a></li>
+      <li><a href="index.html">yasinn</a></li>
+      
     </ul>
   </nav>
+  <?php
+  echo date("") . "<br>";
+  echo date("l jS \of F Y h:i:s A");
+  ?>
 </body>
 </html>
